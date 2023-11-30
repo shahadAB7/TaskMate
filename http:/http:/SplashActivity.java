fff8fdb7e94e3657e5cf8391c31cc8f744b46768
Reham-id10 <152385166+Reham-id10@users.noreply.github.com>
2023-11-30 10:07:28 +0300

    SplashActivity.java

--- http:/http:/SplashActivity.java
+++ http:/http:/SplashActivity.java
@@ -1,1 +1,23 @@
+package net.penguincoders.doit;
 
+import...
+  
+  public class SplashActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreat(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash);
+    getSupportActionBar().hide();
+    
+    final  Intent i=new Intent(packageContext:Splashactivity.this.MainActivity.class);andler9
+    new Handler().postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        startActivity(i);
+        finish();
+      }
+    },delayMillis:1000);
+                              }
+}
+                              
