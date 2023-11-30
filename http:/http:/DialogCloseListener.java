9dbbc38813a2bc5714f4c4ce46f355c3ae026fb9
Reham-id10 <152385166+Reham-id10@users.noreply.github.com>
2023-11-30 18:11:43 +0300

    DialogCloseListener.java

--- http:/http:/DialogCloseListener.java
+++ http:/http:/DialogCloseListener.java
@@ -1,1 +1,8 @@
+package net.penguincoders.doit;
 
+import android.content.DialogInterface;
+
+public interface DialogCloseListener { 
+  
+   public void handleDialogClose(DialogInterface dialog);
+}
