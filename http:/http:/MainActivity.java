6adf8c780c3870167d137aa1bba54f2981209b10
Reham-id10 <152385166+Reham-id10@users.noreply.github.com>
2023-11-30 10:40:03 +0300

    MainActivity.java

--- http:/http:/MainActivity.java
+++ http:/http:/MainActivity.java
@@ -1,1 +1,39 @@
+package net.penguincoders.doit;
 
+import ...
+
+ public class MainActivity extends AppCompatActivity { 
+  
+  private RecyclerView tasksRecyclerView;
+  private ToDoAdapter taskAdapter;
+
+  private List<ToDoModel> taskList;
+
+  
+  @Override 
+protected void onCreate (Bundle savedInstanceState) { 
+  super.onCreate(savedInstanceState); 
+  setContentView(R.layout.activity main);
+  getSupportActionBar().hide();
+
+taskList=new ArrayList<>();
+  
+  tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+  tasksRecyclerView.setLayoutManager(new LinearLayoutManager( context: this));
+  tasksAdapter = new ToDoAdapter( activity: this);
+  tasksRecyclerView.setAdapter(tasksAdapter);
+  
+  ToDoModel task = new ToDoModel();
+  task.setTask("This is a Test Task");
+  task.setStatus(0);
+  task.setId(1);
+  
+  taskList.add(task);
+  taskList.add(task);
+  taskList.add(task);
+  taskList.add(task);
+  taskList.add(task);
+  
+  tasksAdapter.setTasks(taskList);
+}
+}
