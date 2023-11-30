fd6a8ea461da16fd5423df8a8396f51a8e693f30
Reham-id10 <152385166+Reham-id10@users.noreply.github.com>
2023-11-30 11:42:44 +0300

    ToDoAdapter.java

--- http:/http:/ToDoAdapter.java
+++ http:/http:/ToDoAdapter.java
@@ -1,1 +1,45 @@
+package net.penguincoders.doit.Adapter;
 
+import...
+  
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> { 
+  
+  private List<ToDoModel> todoList; 
+  private MainActivity activity; 
+
+  public ToDoAdapter (MainActivity activity){
+    this.activity=activity;
+}
+
+public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){ 
+  View itemView = LayoutInflater.from(parent.getContext())
+    .inflate(R.layout.task layout, parent, attachToRoot: false);
+  return new .ViewHolder(itemView) ;
+}
+
+  public void onBindViewHolder(ViewHolder holder, int position){
+    ToDoModel item = todoList.get(position);
+    holder.task.setText(item.getTask());
+    holder.task.setChecked(toBoolean(item.getStatus()));
+  }
+
+  public int getItemCount(){
+  return todoList.size();
+  }
+    
+    private boolean toBoolean(int n){
+      return n!=0;
+    }
+
+  public void setTasks(List<ToDoModel> todoList){
+    this.todoList=todoList;
+  }
+      public static class ViewHolder extends RecyclerView.ViewHolder{
+        CheckBox task;
+}
+  ViewHolder(View view){
+    super(view);
+    task=view.findViewById(R.id.todoCheckBox);
+  }
+}
+}
